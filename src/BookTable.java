d8720385dc8b18ae5b73c2ff3ca7cee58c4c2afb
Jason Pieterse <jason.azuresky@gmail.com>
2024-01-02 06:31:02 +0200

    Simplifiying database operations into one class
    Made DataSource a singleton.
    Moved almost all the BookTable methods to the DataSource class.

--- src/BookTable.java
+++ src/BookTable.java
@@ -6,89 +6,92 @@ import java.sql.Statement;
 public class BookTable {
     public static final String NAME = "books";
 
-    //Columns
+    // Columns
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_TITLE = "Title";
     public static final String COLUMN_AUTHOR = "Author";
     public static final String COLUMN_QTY = "Qty";
-    
 
-    //Initital Data
-    public static final String STARTING_TITLES [] = {
-        "A Tale of Two Cities",
-        "Harry Potter and the Philosophers Stone",
-        "The Lion, the With & the Wardrobe",
-        "The Lord of the Rings",
-        "Alice in Wonderland",
-        "Queen of Shadows",
-        "Iron Flame",
-        "Throne of Glass",
-        "Heir of Fire",
-        "The Lion: Son of the Forest",
-        "Courage and Honour",
-        "The Queen of Nothing",
-        "The Wicked King",
-        "Everless",
-        "Evermore",
-        "The Cruel Prince",
-        "Empire of Storms",
-        "Harry Potter and the Prisoner of Azkaban",
-        "Harry Potter and the Chamber of Secrets",
-        "Harry Potter and the Goblet of Fire"
+    // Initital Data
+    public static final String STARTING_TITLES[] = {
+            "A Tale of Two Cities",
+            "Harry Potter and the Philosophers Stone",
+            "The Lion, the With & the Wardrobe",
+            "The Lord of the Rings",
+            "Alice in Wonderland",
+            "Queen of Shadows",
+            "Iron Flame",
+            "Throne of Glass",
+            "Heir of Fire",
+            "The Lion: Son of the Forest",
+            "Courage and Honour",
+            "The Queen of Nothing",
+            "The Wicked King",
+            "Everless",
+            "Evermore",
+            "The Cruel Prince",
+            "Empire of Storms",
+            "Harry Potter and the Prisoner of Azkaban",
+            "Harry Potter and the Chamber of Secrets",
+            "Harry Potter and the Goblet of Fire"
     };
 
-    public static final String STARTING_AUTHORS [] = {
-        "Charles Dickens",
-        "J.K. Rowling",
-        "C.S. Lewis",
-        "J.R.R Tolkien",
-        "Lewis Carrol",
-        "Sarah J Maas",
-        "Rebecca Yarros",
-        "Sarah J Maas",
-        "Sarah J Maas",
-        "Mike Brooks",
-        "Graham McNeill",
-        "Holly Black",
-        "Holly Black",
-        "Sara Holland",
-        "Sara Holland",
-        "Holly Black",
-        "Sarah J Maas",
-        "J.K. Rowling",
-        "J.K. Rowling",
-        "J.K. Rowling"
+    public static final String STARTING_AUTHORS[] = {
+            "Charles Dickens",
+            "J.K. Rowling",
+            "C.S. Lewis",
+            "J.R.R Tolkien",
+            "Lewis Carrol",
+            "Sarah J Maas",
+            "Rebecca Yarros",
+            "Sarah J Maas",
+            "Sarah J Maas",
+            "Mike Brooks",
+            "Graham McNeill",
+            "Holly Black",
+            "Holly Black",
+            "Sara Holland",
+            "Sara Holland",
+            "Holly Black",
+            "Sarah J Maas",
+            "J.K. Rowling",
+            "J.K. Rowling",
+            "J.K. Rowling"
     };
 
-    public static final int STARTING_QTY [] = {
-        30,
-        40,
-        25,
-        37,
-        12,
-        154,
-        122,
-        87,
-        164,
-        11,
-        11,
-        39,
-        29,
-        56,
-        57,
-        30,
-        734,
-        41,
-        61,
-        91
+    public static final int STARTING_QTY[] = {
+            30,
+            40,
+            25,
+            37,
+            12,
+            154,
+            122,
+            87,
+            164,
+            11,
+            11,
+            39,
+            29,
+            56,
+            57,
+            30,
+            734,
+            41,
+            61,
+            91
     };
 
     /**
-     * Executes a query using the given Statement object to cleanly (re)create the table in the database.
-     * Can safely be call even if the table already exists but existing data will be destroyed.
+     * Executes a query using the given Statement object to cleanly (re)create the
+     * table in the database.
+     * Can safely be call even if the table already exists but existing data will be
+     * destroyed.
      *
-     * This method assumes that the correct default database has been set with "USE database_name;".
-     * No prepared statements are used here because there is no user input to sanitise and this is a query
+     * This method assumes that the correct default database has been set with "USE
+     * database_name;".
+     * No prepared statements are used here because there is no user input to
+     * sanitise and this is a query
      * that should run very infrequently.
      *
      * @param connection A Connection object from the database connection.
@@ -97,41 +100,47 @@ public class BookTable {
         Statement statement = connection.createStatement();
         StringBuilder queryBuilder = new StringBuilder();
 
-        //Remove the table first if it exists to prevent a SQL error if it's already there.
+        // Remove the table first if it exists to prevent a SQL error if it's already
+        // there.
         queryBuilder.append("DROP TABLE IF EXISTS ").append(NAME).append(";");
         statement.executeUpdate(queryBuilder.toString());
         queryBuilder.setLength(0);
 
         queryBuilder.append("CREATE TABLE ").append(NAME).append("(\n")
-                    .append("\t").append(COLUMN_ID)    .append(" INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,\n")
-                    .append("\t").append(COLUMN_TITLE) .append(" VARCHAR(80) NOT NULL,\n")
-                    .append("\t").append(COLUMN_AUTHOR).append(" VARCHAR(40) NOT NULL,\n")
-                    .append("\t").append(COLUMN_QTY)   .append(" INT DEFAULT 0\n")
-                    .append(");");
+                .append("\t").append(COLUMN_ID).append(" INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,\n")
+                .append("\t").append(COLUMN_TITLE).append(" VARCHAR(80) NOT NULL,\n")
+                .append("\t").append(COLUMN_AUTHOR).append(" VARCHAR(40) NOT NULL,\n")
+                .append("\t").append(COLUMN_QTY).append(" INT DEFAULT 0\n")
+                .append(");");
         statement.executeUpdate(queryBuilder.toString());
         queryBuilder.setLength(0);
 
-        //The task stipulates that the ID numbers should start from 3001;
+        // The task stipulates that the ID numbers should start from 3001;
         queryBuilder.append("ALTER TABLE ").append(NAME).append(" AUTO_INCREMENT = 3001;");
         statement.executeUpdate(queryBuilder.toString());
         queryBuilder.setLength(0);
     }
 
     /**
-     * Executes a query using the given Statement object to add a new book to the database with the given
+     * Executes a query using the given Statement object to add a new book to the
+     * database with the given
      * parameters
      *
-     * This method assumes that the correct default database has been set with "USE database_name;" and that the book
-     * table already exists. This method uses prepared statements to handle sanitising of the user input to prevent
+     * This method assumes that the correct default database has been set with "USE
+     * database_name;" and that the book
+     * table already exists. This method uses prepared statements to handle
+     * sanitising of the user input to prevent
      * SQL injection attacks.
      *
-     * The return value is the ID of the new record, which is an auto incrementing column so the new value is found
-     *  as the max of the ID column.
+     * The return value is the ID of the new record, which is an auto incrementing
+     * column so the new value is found
+     * as the max of the ID column.
      *
      * @param connection The database connection object.
-     * @param title The title of the new book. (Does not need to be unique in the database)
-     * @param author The author of the book
-     * @param qty The  initial quantity of the book in stock.
+     * @param title      The title of the new book. (Does not need to be unique in
+     *                   the database)
+     * @param author     The author of the book
+     * @param qty        The initial quantity of the book in stock.
      *
      * @returns The ID of the new record or -1 on failure
      *
@@ -153,16 +162,16 @@ public class BookTable {
         }
         return newID;
     }
-    
+
     /**
      * Inserts a new record into the books table using the data provided.
      *
      * @param connection The database {@link Connection} object
-     * @param newBook A {@link Book} object that will be inserted.
+     * @param newBook    A {@link Book} object that will be inserted.
      *
      * @returns {@code true on success}
      */
-    public static int insertBook(Connection connection, Book newBook) throws SQLException{
+    public static int insertBook(Connection connection, Book newBook) throws SQLException {
         return insertBook(connection, newBook.title, newBook.author, newBook.qty);
     }
 
@@ -177,7 +186,7 @@ public class BookTable {
             return -1;
         }
     }
-    
+
     /**
      * Inserts the initial data into the books table.
      *
@@ -188,18 +197,18 @@ public class BookTable {
         boolean result = true;
 
         for (int index = 0; index < STARTING_TITLES.length; ++index) {
-            boolean success = insertBook (
-                connection, STARTING_TITLES[index],
-                STARTING_AUTHORS[index],
-                STARTING_QTY[index]
-            ) > 0;
+            boolean success = insertBook(
+                    connection, STARTING_TITLES[index],
+                    STARTING_AUTHORS[index],
+                    STARTING_QTY[index]) > 0;
             result = result && success;
         }
         return result;
     }
-    
+
     /**
-     * Convenience method to be used after the database is created initially. Creates the 
+     * Convenience method to be used after the database is created initially.
+     * Creates the
      * books table and inserts the initial data.
      *
      * @returns {@code true} on success.
@@ -208,19 +217,20 @@ public class BookTable {
         createTable(connection);
         return insertInitialData(connection);
     }
-    
+
     /**
-     * Checks to see if the Books table exists. This method assumes the database already exists.
+     * Checks to see if the Books table exists. This method assumes the database
+     * already exists.
      * A SQL Exception will be thrown if the database doesn't exist yet.
      *
      * @param connection The database {@link Connection} object
      * @returns {@code true} if the database table exists.
      */
-    public static boolean tableExists(Connection connection) throws SQLException{
+    public static boolean tableExists(Connection connection) throws SQLException {
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("SELECT * FROM information_schema.tables WHERE table_schema = '")
-            .append(DataSource.getDatabaseName()).append("' AND table_name = '")
-            .append(NAME).append("';");
+                .append(DataSource.getDatabaseName()).append("' AND table_name = '")
+                .append(NAME).append("';");
         Statement statement = connection.createStatement();
         ResultSet result = statement.executeQuery(queryBuilder.toString());
 
@@ -238,12 +248,12 @@ public class BookTable {
     public static boolean deleteBook(Connection connection, int idToDelete) throws SQLException {
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("DELETE FROM ").append(NAME)
-            .append(" WHERE ").append(COLUMN_ID).append(" = ")
-            .append(idToDelete).append(';');
+                .append(" WHERE ").append(COLUMN_ID).append(" = ")
+                .append(idToDelete).append(';');
         Statement statement = connection.createStatement();
         return statement.executeUpdate(queryBuilder.toString()) > 0;
     }
-    
+
     /**
      * Helper method to create a template for UPDATE query Statements.
      *
@@ -253,12 +263,13 @@ public class BookTable {
     private static String getUpdateQuery(String column) {
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("UPDATE ").append(NAME).append(" SET ").append(column)
-                    .append(" = ? WHERE ").append(COLUMN_ID).append(" = ?;");
+                .append(" = ? WHERE ").append(COLUMN_ID).append(" = ?;");
         return queryBuilder.toString();
     }
-    
+
     /**
-     * Updates a record in the books table of the given ID number by changing the title.
+     * Updates a record in the books table of the given ID number by changing the
+     * title.
      * 
      * @param connection The database {@link Connection} object
      * @param idToChange The ID number of the record to update.
@@ -266,42 +277,44 @@ public class BookTable {
      *
      * @returns {@code true} on success
      */
-    public static boolean updateTitle(Connection connection, int idToChange,String newTitle) throws SQLException{
+    public static boolean updateTitle(Connection connection, int idToChange, String newTitle) throws SQLException {
         PreparedStatement statement = connection.prepareStatement(getUpdateQuery(COLUMN_TITLE));
-        statement.setString(1, newTitle);        
+        statement.setString(1, newTitle);
         statement.setInt(2, idToChange);
-        return statement.executeUpdate() >0;
+        return statement.executeUpdate() > 0;
     }
 
     /**
-     * Updates a record in the books table of the given ID number by changing the title.
+     * Updates a record in the books table of the given ID number by changing the
+     * title.
      * 
      * @param connection The database {@link Connection} object
      * @param idToChange The ID number of the record to update.
-     * @param newAuthor   The new book author.
+     * @param newAuthor  The new book author.
      *
      * @returns {@code true} on success
      */
-    public static boolean updateAuthor(Connection connection, int idToChange,String newAuthor) throws SQLException{
+    public static boolean updateAuthor(Connection connection, int idToChange, String newAuthor) throws SQLException {
         PreparedStatement statement = connection.prepareStatement(getUpdateQuery(COLUMN_AUTHOR));
-        statement.setString(1, newAuthor);        
+        statement.setString(1, newAuthor);
         statement.setInt(2, idToChange);
-        return statement.executeUpdate() >0;
+        return statement.executeUpdate() > 0;
     }
 
     /**
-     * Updates a record in the books table of the given ID number by changing the title.
+     * Updates a record in the books table of the given ID number by changing the
+     * title.
      * 
      * @param connection The database {@link Connection} object
      * @param idToChange The ID number of the record to update.
-     * @param newQty   The new quantity.
+     * @param newQty     The new quantity.
      *
      * @returns {@code true} on success
      */
-    public static boolean updateQty(Connection connection, int idToChange, int newQty) throws SQLException{
+    public static boolean updateQty(Connection connection, int idToChange, int newQty) throws SQLException {
         PreparedStatement statement = connection.prepareStatement(getUpdateQuery(COLUMN_QTY));
-        statement.setInt(1, newQty);        
+        statement.setInt(1, newQty);
         statement.setInt(2, idToChange);
-        return statement.executeUpdate() >0;
+        return statement.executeUpdate() > 0;
     }
 }
--- src/DataSource.java
+++ src/DataSource.java
@@ -9,19 +9,26 @@ class DataSource {
     private static final String DATABASE_VENDOR = "mysql";
     private static final String DATABASE_HOST = "localhost";
     private static final String DATABASE_PORT = "3306";
-    private static final String DATABASE_USER = "Jason";
+    private static final String DATABASE_USER = "Jason"; //TODO: Replace username and password
     private static final String DATABASE_PASSWORD = "KochiraDozo";
     
     private Connection connection;
+
+    // Singleton pattern (instance, inctance-getter & private constructor)
+    //-----
+    public static final DataSource instance = new DataSource();
     
-    public static String getDatabaseName() {
-        return DATABASE_NAME;
-    }
+    public static DataSource getInstance() { return instance; }
 
-    public Connection getConnection() {
-        return connection;
+    private DataSource() throws SQLException {
+        connection = DriverManager.getConnection(
+            getConnectionURL(),
+            DATABASE_USER, DATABASE_PASSWORD
+        );
+        initialiseDatabase();
     }
-
+    //------
+    
     private String getConnectionURL() {
         StringBuilder connectionURL = new StringBuilder();
         connectionURL.append(DATABASE_PROTOCOL).append(':')
@@ -31,15 +38,7 @@ class DataSource {
             .append(DATABASE_NAME).append("?useSSL=false");
         return connectionURL.toString();
     }
-
-    public DataSource() throws SQLException {
-        connection = DriverManager.getConnection(
-            getConnectionURL(),
-            DATABASE_USER, DATABASE_PASSWORD //TODO: Create a system user.
-        );
-        initialiseDatabase();
-    }
-
+    
     private boolean databaseExists() {
         Statement statement = connection.createStatement;
         StringBuilder query = new StringBuilder();
@@ -61,17 +60,256 @@ class DataSource {
     public void initialiseDatabase() {
         if (!databaseExists()) {
             createDatabase();   
-            setDatabaseAsDefault();
-            BookTable.initialiseBookTable(connection);
-        } else {
-            setDatabaseAsDefault();
-            if (!BookTable.tableExists(connection)) {
-                BookTable.initialiseBookTable(connection);
-            }
+        } 
+        setDatabaseAsDefault();
+        if (bookTableExists()) {
+            initialiseBookTable();
         }
+        
     }
 
     public void deleteBook (Book book) throws SQLException {
         BookTable.deleteBook(connection, book.id);
+    }
+
+    //Book table paramaters
+    //======================
+    private static final String BOOK_TABLE_NAME = "books";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "Title";
+    public static final String COLUMN_AUTHOR = "Author";
+    public static final String COLUMN_QTY = "Qty";
+
+    /**
+     * Executes a query using the given Statement object to cleanly (re)create the table in the database.
+     * Can safely be call even if the table already exists but existing data will be destroyed.
+     *
+     * This method assumes that the correct default database has been set with "USE database_name;".
+     * No prepared statements are used here because there is no user input to sanitise and this is a query
+     * that should run very infrequently.
+     *
+     */
+    private void createBookTable() throws SQLException {
+        Statement statement = connection.createStatement();
+        StringBuilder queryBuilder = new StringBuilder();
+
+        //Remove the table first if it exists to prevent a SQL error if it's already there.
+        queryBuilder.append("DROP TABLE IF EXISTS ").append(BOOK_TABLE_NAME).append(";");
+        statement.executeUpdate(queryBuilder.toString());
+        queryBuilder.setLength(0);
+
+        queryBuilder.append("CREATE TABLE ").append(BOOK_TABLE_NAME).append("(\n")
+                    .append("\t").append(COLUMN_ID)    .append(" INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,\n")
+                    .append("\t").append(COLUMN_TITLE) .append(" VARCHAR(80) NOT NULL,\n")
+                    .append("\t").append(COLUMN_AUTHOR).append(" VARCHAR(40) NOT NULL,\n")
+                    .append("\t").append(COLUMN_QTY)   .append(" INT DEFAULT 0\n")
+                    .append(");");
+        statement.executeUpdate(queryBuilder.toString());
+        queryBuilder.setLength(0);
+
+        //The task stipulates that the ID numbers should start from 3001;
+        queryBuilder.append("ALTER TABLE ").append(BOOK_TABLE_NAME).append(" AUTO_INCREMENT = 3001;");
+        statement.executeUpdate(queryBuilder.toString());
+        queryBuilder.setLength(0);
+    }
+
+    /**
+     * Convenience method to be used after the database is created initially.
+     * Creates the
+     * books table and inserts the initial data.
+     *
+     * @returns {@code true} on success.
+     */
+    public boolean initialiseBookTable() throws SQLException {
+        createBookTable();
+        return insertInitialData();
+    }
+    
+    /**
+     * Executes a query using the given Statement object to add a new book to the
+     * database with the given
+     * parameters
+     *
+     * This method assumes that the correct default database has been set with "USE
+     * database_name;" and that the book
+     * table already exists. This method uses prepared statements to handle
+     * sanitising of the user input to prevent
+     * SQL injection attacks.
+     *
+     * The return value is the ID of the new record, which is an auto incrementing
+     * column so the new value is found
+     * as the max of the ID column.
+     *
+     * @param connection The database connection object.
+     * @param title      The title of the new book. (Does not need to be unique in the database)
+     * @param author     The author of the book
+     * @param qty        The initial quantity of the book in stock.
+     *
+     * @returns The ID of the new record or -1 on failure
+     *
+     */
+    public int insertBook(String title, String author, int qty) throws SQLException {
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("INSERT INTO ").append(BOOK_TABLE_NAME).append(" (")
+                .append(COLUMN_TITLE).append(", ")
+                .append(COLUMN_AUTHOR).append(", ")
+                .append(COLUMN_QTY).append(") VALUES (?, ?, ?);");
+        PreparedStatement statement = connection.prepareStatement(queryBuilder.toString());
+        statement.setString(1, title);
+        statement.setString(1, author);
+        statement.setInt(1, qty);
+        int rowsAffected = statement.executeUpdate();
+        int newID = -1;
+        if (rowsAffected > 0) {
+            newID = getMaxID();
+        }
+        return newID;
+    }
+
+    /**
+     * Inserts a new record into the books table using the data provided.
+     *
+     * @param newBook    A {@link Book} object that will be inserted.
+     *
+     * @returns {@code true on success}
+     */
+    public int insertBook( Book newBook) throws SQLException {
+        return insertBook(newBook.title, newBook.author, newBook.qty);
+    }
+
+    public int getMaxID() throws SQLException {
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("SELECT MAX(").append(COLUMN_ID).append(") FROM ").append(BOOK_TABLE_NAME).append(';');
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(queryBuilder.toString());
+        if (result.next()) {
+            return result.getInt(1);
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Checks to see if the Books table exists. This method assumes the database
+     * already exists.
+     * A SQL Exception will be thrown if the database doesn't exist yet.
+     *
+     * @returns {@code true} if the database table exists.
+     */
+    public boolean bookTableExists() throws SQLException {
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("SELECT * FROM information_schema.tables WHERE table_schema = '")
+                .append(DATABASE_NAME).append("' AND table_name = '")
+                .append(BOOK_TABLE_NAME).append("';");
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(queryBuilder.toString());
+
+        return result.next();
+    }
+
+    /**
+     * Deletes a record in the books table of the given ID number.
+     * 
+     * @param idToDelete The ID number of the record to delete.
+     *
+     * @returns {@code true} on success
+     */
+    public boolean deleteBook(int idToDelete) throws SQLException {
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("DELETE FROM ").append(BOOK_TABLE_NAME)
+                .append(" WHERE ").append(COLUMN_ID).append(" = ")
+                .append(idToDelete).append(';');
+        Statement statement = connection.createStatement();
+        return statement.executeUpdate(queryBuilder.toString()) > 0;
+    }
+
+    //Initital Data
+    private final String STARTING_TITLES [] = {
+        "A Tale of Two Cities",
+        "Harry Potter and the Philosophers Stone",
+        "The Lion, the With & the Wardrobe",
+        "The Lord of the Rings",
+        "Alice in Wonderland",
+        "Queen of Shadows",
+        "Iron Flame",
+        "Throne of Glass",
+        "Heir of Fire",
+        "The Lion: Son of the Forest",
+        "Courage and Honour",
+        "The Queen of Nothing",
+        "The Wicked King",
+        "Everless",
+        "Evermore",
+        "The Cruel Prince",
+        "Empire of Storms",
+        "Harry Potter and the Prisoner of Azkaban",
+        "Harry Potter and the Chamber of Secrets",
+        "Harry Potter and the Goblet of Fire"
+    };
+
+    private final String STARTING_AUTHORS [] = {
+        "Charles Dickens",
+        "J.K. Rowling",
+        "C.S. Lewis",
+        "J.R.R Tolkien",
+        "Lewis Carrol",
+        "Sarah J Maas",
+        "Rebecca Yarros",
+        "Sarah J Maas",
+        "Sarah J Maas",
+        "Mike Brooks",
+        "Graham McNeill",
+        "Holly Black",
+        "Holly Black",
+        "Sara Holland",
+        "Sara Holland",
+        "Holly Black",
+        "Sarah J Maas",
+        "J.K. Rowling",
+        "J.K. Rowling",
+        "J.K. Rowling"
+    };
+
+    private final int STARTING_QTY [] = {
+        30,
+        40,
+        25,
+        37,
+        12,
+        154,
+        122,
+        87,
+        164,
+        11,
+        11,
+        39,
+        29,
+        56,
+        57,
+        30,
+        734,
+        41,
+        61,
+        91
+    };
+
+    /**
+     * Inserts the initial data into the books table.
+     *
+     * @returns {@code true} on success.
+     */
+    public boolean insertInitialData() throws SQLException {
+        boolean result = true;
+
+        for (int index = 0; index < STARTING_TITLES.length; ++index) {
+            boolean success = insertBook (
+                STARTING_TITLES[index],
+                STARTING_AUTHORS[index],
+                STARTING_QTY[index]
+            ) > 0;
+            result = result && success;
+        }
+        return result;
     }
 }
